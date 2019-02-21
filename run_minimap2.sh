15d5adf3046ed0f70501ae35217c89a7ebd1bdca
Ken Cho <cholingken@gmail.com>
2019-02-21 10:09:26 +0800

    change path

--- run_minimap2.sh
+++ run_minimap2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 name=wgs_hkcgs_demo_DMD_2aug
-ref=/nas4/rma/working/sv/ref/hs37d5.fa.mmi
+ref=/working/sv/ref/hs37d5.fa.mmi
 fastq=${name}.fastq.gz
-minimap2=/nas4/rma/working/minion_project/tools/minimap2/minimap2
+minimap2=/working/minion_project/tools/minimap2/minimap2
 bam=${name}_minimap2.bam
 
 $minimap2 -t 48 --MD -a $ref $fastq | samtools sort -@ 48 -o $bam - && samtools index -@ 48 $bam
